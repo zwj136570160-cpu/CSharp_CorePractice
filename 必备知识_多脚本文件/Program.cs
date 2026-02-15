101a3facfa2a7e148a7272fa8ef137bc35239013
ZWJ136570160-CPU <zwj136570160@gmail.com>
2026-02-15 12:42:45 +0800

    添加项目文件。

---
+++ 必备知识_多脚本文件/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace 必备知识_多脚本文件
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("多脚本文件");
+
+            #region 1.了解脚本文件格式和路径
+
+            #endregion
+        }
+    }
+}
