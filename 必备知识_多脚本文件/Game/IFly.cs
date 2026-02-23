180bc560c4aa2cc5737d856c5b2f0b16c413d32c
ZWJ136570160-CPU <zwj136570160@gmail.com>
2026-02-23 12:52:22 +0800

    多脚本任务

---
+++ 必备知识_多脚本文件/Game/IFly.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 必备知识_多脚本文件.Game
+{
+    internal class IFly
+    {
+    }
+}
--- 必备知识_多脚本文件/Program.cs
+++ 必备知识_多脚本文件/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace 必备知识_多脚本文件
+﻿using 必备知识_多脚本文件.Game;
+
+namespace 必备知识_多脚本文件
 {
     internal class Program
     {
@@ -9,6 +11,19 @@
             #region 1.了解脚本文件格式和路径
 
             #endregion
+
+            #region 2.新建脚本文件
+
+            #endregion
+
+            #region 3.在文件夹中新建脚本文件
+            GameObject obj = new GameObject();
+            #endregion
         }
     }
+    //总结：
+    //1.C#脚本文件的后缀是.cs
+    //2.C#解决方案中的一些重要文件夹 bin
+    //3.解决方案资源管理器窗口，工程右键点添加
+    //4.文件夹中新建脚本文件，需要注意命名空间名的改变
 }
